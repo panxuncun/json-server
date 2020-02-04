9d644860b6655219d275ed7215f230ae02d0a0da
潘 <666@qq.com>
2020-02-05 02:54:20 +0800

    第四版：1.实现Mock和json-server的组合 2.实现随机图片 3.todo：省市数据填进去 4.如果要配置的话，新建json-server.json然后运行json-server -c json-server.json db.js 5.有最后效果图，忘记启动命令的话可以看一下

--- db.js
+++ db.js
@@ -1,23 +1,26 @@
 
 var Mock = require('mockjs')
+
+var img = Mock.Random.image('100x100', '#4A7BF7', 'Hello')
 //import Mock from 'mockjs' 错
-var mdata = Mock.mock({
-    // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
-    'list|1-10': [{
-        // 属性 id 是一个自增数，起始值为 1，每次增 1
-        'id|+1': 1,
-        'name':'@cname'
-    }]
+var data = Mock.mock({
+  // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+  'user|5-10': [{
+    // 属性 id 是一个自增数，起始值为 1，每次增 1
+    'id|+1': 1,
+    'name': '@cfirst@cname',
+    'province': '@province',
+    'img': Mock.Random.image('80x80', '#4A7BF7', '@name')
+  }]
 })
+
+
+//console.log(Mock.Random.image('100x100', '#4A7BF7', 'Hello'))
+
 // 输出结果
-console.log(JSON.stringify(mdata, null, 4))
+//console.log(JSON.stringify(mdata, null, 4))
 
-module.exports = function() {
-  let data = { list: [],mdata }
-
-
-//   for (let i = 0; i < 100; i++) {
-//     data.list.push({ id: i, name: 'user' + i })
-//   }
-  return mdata
+module.exports = function () {
+  return data
 }
+
